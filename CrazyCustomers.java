ffb1f277b86d650307e0bbabff6f126248dec473
aichamorabit0-commits <aichamorabit0@gmail.com>
2025-11-07 10:52:41 +0100

    Add files via upload

---
+++ CrazyCustomers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CrazyCustomers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("wie viele Muffins sind verfügbar?");
+        int muffins=scanner.nextInt();
+        int Student=1;
+        int Std1=3;
+        int Std2=5;
+        if(muffins>Std1){
+            System.out.println("studierende"+Student+":"+Std1+"muffins");
+            else{ System.out.println("nicht genug muffins vorhanden");muffins-=Std1;Student++;}
+            
+
+
+
+
